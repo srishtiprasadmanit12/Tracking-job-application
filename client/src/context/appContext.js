4c8042cb2f9861784405621e893d53262dea87e6
srishti <srishtiprasad500@gmail.com>
2022-07-11 20:34:12 +0530

    complete register user from

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,7 +34,7 @@
 
 import React, {useState,useReducer,useContext,registerUser} from 'react'
 import reducer from './reducer'
-
+import axios from 'axios'
 import {DISPLAY_ALERT,CLEAR_ALERT,REGISTER_USER_BEGIN,REGISTER_USER_SUCCESS,REGISTER_USER_ERROR} from './actions'
 import { clear } from '@testing-library/user-event/dist/clear'
 const initialState={
@@ -44,7 +44,8 @@ alertText:'',
 alertType:'',
 user:null,
 taken:null,
-userLocation:''
+userLocation:'',
+jobLocation:''
 }
 
 const AppContext=React.createContext()
@@ -62,9 +63,28 @@ const AppProvider=({children})=>{
         dispatch({type:CLEAR_ALERT})
       },3000)
     }
-
+    
     const registerUser=async (currentUser)=>{
-      console.log(currentUser)
+      // console.log(currentUser)
+      dispatch({type:REGISTER_USER_BEGIN})
+      try{
+        const response=await axios.post('/api/v1/auth/register',currentUser)
+        console.log(response);
+        const {user,token,location}=response.data
+        dispatch({
+          type:REGISTER_USER_SUCCESS,
+          payload:{user,token,location},
+        })
+        //local storage later
+      }catch(error){
+       console.log(error.response)
+       dispatch({
+        type:REGISTER_USER_ERROR,
+        payload:{msg:error.response.data.msg},
+      
+      })
+      }
+      clearAlert()
     }
 
     return (
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -16,11 +16,50 @@
 
 const reducer = (state, action) => {
     if(action.type===DISPLAY_ALERT){
-        return {...state,showAlert:true ,alertType:'danger',alertText:'please provide all values!'}
+        return {
+            ...state,
+            showAlert:true ,
+            alertType:'danger',
+            alertText:'please provide all values!'
+        }
     }
 
     if(action.type===CLEAR_ALERT){
         return {...state,showAlert:false ,alertType:'',alertText:''}
+    }
+    if(action.type===REGISTER_USER_BEGIN)
+    {
+        return {...state,isLoading:true}
+    }
+    if(action.type===REGISTER_USER_SUCCESS)
+    {
+        return {
+            ...state,
+            isLoading:false,
+            token:action.payload.token,
+            user:action.payload.user,
+            userLocation:action.payload.location,
+            jobLocation:action.payload.location,
+            showAlert:true,
+            alertType:'success',
+            alertText:'User created! Redirecting...',
+
+        }
+    }
+    if(action.type===REGISTER_USER_ERROR)
+    {
+        return {
+            ...state,
+            isLoading:false,
+            // token:action.payload.token,
+            // user:action.payload.user,
+            // userLocation:action.payload.location,
+            // jobLocation:action.payload.location,
+            showAlert:true,
+            alertType:'danger',
+            alertText:action.payload.msg,
+
+        }
     }
     throw new Error(`no such action :${action.type}`)
   }
